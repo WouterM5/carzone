323868d0cd5ef38d6a9ef8b0223d89cc212c85ff
Wouter <wouter.meus@hotmail.com>
2022-10-09 13:22:05 +0200

    admin setup

--- pages/admin.py
+++ pages/admin.py
@@ -1,3 +1,29 @@
+import imp
 from django.contrib import admin
+from pages.models import Teams
+from django.utils.html import format_html
 
 # Register your models here.
+
+
+class TeamAdmin(admin.ModelAdmin):
+    def thumbnail(self, object):
+        return format_html('<img src="{}" width="60" style="border-radius:50%;" />'.format(object.photo.url))
+    
+    thumbnail.short_description = 'photo'
+
+    list_display = ('id','thumbnail','first_name', 'last_name', 'designation','created_date',)
+    list_display_links = ('id','thumbnail','first_name',)
+    search_fields = ('first_name', 'last_name', 'designation',)
+    list_filter = ('designation','first_name', 'last_name',)
+    ordering = ('id',)
+    class Meta():
+        ordering = ('first_name', 'last_name')
+
+
+
+    
+
+
+
+admin.site.register(Teams, TeamAdmin,)
