b5fbe9b75090844af194c24a4dcdb21e1ed8830f
Wouter <wouter.meus@hotmail.com>
2022-10-14 15:12:26 +0200

    14/10

--- cars/urls.py
+++ cars/urls.py
@@ -1,6 +1,9 @@
+from unicodedata import name
 from django.urls import path
 from . import views
 
 urlpatterns = [
-    path('', views.cars, name='cars')
+    path('', views.cars, name='cars'),
+    path('<int:id>', views.car_detail, name='car_detail'),
+    path('search', views.search, name='search')
 ]
--- pages/views.py
+++ pages/views.py
@@ -1,15 +1,32 @@
 from django.shortcuts import render
 from cars.models import Car
+from cars.views import search
 from pages.models import Teams
+from django.core.paginator import EmptyPage, PageNotAnInteger, Paginator
 
 # Create your views here.
 
 def home(request):
     featured_cars = Car.objects.order_by('-created_date').filter(is_featured=True)
     latest_cars = Car.objects.order_by('-created_date')
+    paginator = Paginator(latest_cars, 3)
+    page = request.GET.get('page')
+    paged_cars = paginator.get_page(page)
+    #search_fields = Car.objects.values('model','city', 'year', 'body_style')
+    model_search = Car.objects.values_list('model', flat=True).distinct()
+    city_search = Car.objects.values_list('city', flat=True).distinct()
+    year_search = Car.objects.values_list('year', flat=True).distinct()
+    body_style_search = Car.objects.values_list('body_style', flat=True).distinct()
+
     data = {
         'featured_cars': featured_cars,
-        'latest_cars': latest_cars,
+        'latest_cars': paged_cars,
+       # 'search_fields': search_fields
+        'model_search': model_search,
+        'city_search': city_search,
+        'year_search': year_search,
+        'body_style_search': body_style_search
+
     }
     return render(request,'pages/home.html', data)
 
